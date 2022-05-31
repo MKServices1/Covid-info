21d7714f9417a001d7d81f22553d7200bd2b9f32
Imran Ali Khan <70568289+MKServices1@users.noreply.github.com>
2022-05-31 16:10:05 +0500

    Create app_url.dart

---
+++ services/utilities/app_url.dart
@@ -0,0 +1,9 @@
+import 'dart:convert';
+
+class AppUrl{
+
+  static const String baseUrl="https://disease.sh/v3/covid-19/";
+
+  static const String worldStatesApi= baseUrl+"all";
+  static const String countriesList= baseUrl+"countries";
+}
